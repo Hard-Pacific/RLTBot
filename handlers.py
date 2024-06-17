5001ff7b991f503209f46fad8c7253a8c0086cd5
HardPacific <leedmytriy@gmail.com>
2024-06-17 07:32:06 +0300

    Update handlers.py
    Агрегируем данные полученные от пользователя в телеграм, пока не в необходимом формате

--- handlers.py
+++ handlers.py
@@ -1,7 +1,54 @@
 from aiogram import types
 from aiogram.filters import CommandStart
 from loader import bot, dp
+from pymongo import MongoClient
+from datetime import datetime
+from datetime import timedelta
+import json
 
 @dp.message(CommandStart())
 async def start(message: types.Message):
     await bot.send_message(message.from_user.id, "Здравствуйте, я существую!")
+
+@dp.message()
+async def agregate(message: types.Message):
+        # Подключаемся к sampleDB
+        client = MongoClient("localhost", 27017)
+        db = client["sampleDB"]
+        collection = db["sample_collection"]
+        input_text = message.text
+
+        # Преобразуем сообщение от пользователя в json
+        # Получаем dt_from, dt_upto, group_type
+        input_json = json.loads(input_text)
+        dt_from = datetime.fromisoformat(input_json["dt_from"])
+        dt_upto = datetime.fromisoformat(input_json["dt_upto"])
+        group_type = input_json["group_type"].replace("day", "dayOfYear")
+
+        # Рассматриваем наш временной промежуток от dt_from до dt_upto
+        # Считаем сумы по group_type
+        # Сортируем по датам и возвращаем наши данные
+        documents = collection.aggregate([
+                {"$match": {
+                        "dt": {
+                                "$gte": dt_from,
+                                "$lt": dt_upto + timedelta(seconds=1)
+                                }
+                        }
+                },
+                {"$group": {
+                        "_id": {
+                                f"{group_type}": {f"${group_type}": "$dt"}
+                                },
+                        "total": {"$sum": "$value"},
+                        "label": {"$first": "$dt"},
+                        }
+                },
+                {"$sort": {"_id": 1}},
+                {"$project": {
+                        "_id": 1,
+                        "label": 1,
+                        "total": 1
+                        }
+                }
+        ])
